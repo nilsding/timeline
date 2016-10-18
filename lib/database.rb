81257281c38d8a4f06c70d2067919ada6c4c2adf
Georg G <nilsding@nilsding.org>
2016-10-18 10:06:54 +0200

    Add reset_{tweets,posts} tasks, dosql method

--- lib/database.rb
+++ lib/database.rb
@@ -1,6 +1,13 @@
 require 'sqlite3'
 
 $db = SQLite3::Database.new(File.expand_path("../../data/simulator.db", __FILE__))
+$db_output_color = 35
+
+def dosql(sql, args = nil, desc = "SQL")
+  puts "\033[34;1m  #{desc}  \033[#{$db_output_color};1m #{sql}   \033[0;1m#{args.nil? ? '' : args.inspect}\033[0m" if $bot[:config][:debug]
+  $db_output_color = $db_output_color == 35 ? 36 : 35
+  $db.execute(sql, args)
+end
 
 Twittbot::BotPart.new :database do
   on :load do
@@ -23,8 +30,17 @@ SQL
       CREATE TABLE IF NOT EXISTS posts (
         id INTEGER PRIMARY KEY,
         text TEXT,
-        created_at INTEGER
+        created_at INTEGER,
+        tweet_id INTEGER
       );
 SQL
+  end
+
+  task :reset_tweets, desc: 'Reset the tweets database' do
+    dosql("DELETE FROM tweets;")
+  end
+
+  task :reset_posts, desc: 'Reset the posts database' do
+    dosql("DELETE FROM posts;")
   end
 end
