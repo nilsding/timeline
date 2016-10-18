bc7aadc08168680e76c1e9621052b4485c387696
Georg G <nilsding@nilsding.org>
2016-10-18 11:11:24 +0200

    Use RubyMarkovify::ArrayText instead of our own class

--- lib/generate.rb
+++ lib/generate.rb
@@ -1,15 +1,9 @@
 require 'ruby_markovify'
 
 Twittbot::BotPart.new :generate do
-  class SimulatorText < RubyMarkovify::Text
-    def sentence_split(text)
-      text.split("\x00")
-    end
-  end
-
   task :generate, desc: 'Tweets something from the markov chain' do
     dataset = fetch_tweets
-    model = SimulatorText.new(dataset, state_size = 2)
+    model = RubyMarkovify::ArrayText.new(dataset, state_size = 2)
     retries = 100
     while retries > 0
       tweet = model.make_short_sentence(140, tries: 100)
@@ -24,7 +18,7 @@ Twittbot::BotPart.new :generate do
 
   def fetch_tweets
     rows = dosql("SELECT id, text FROM tweets ORDER BY id DESC LIMIT 150", nil, "Tweet Load")
-    rows.map{ |row| row[1] }.join("\x00")
+    rows.map{ |row| row[1] }
   end
 
   def unique?(text)
